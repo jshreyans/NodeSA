616e4b517fffe36c11f93a12f4a4134341597a70
Shreyans Jain <shreyansj551@gmail.com>
2019-01-11 00:05:42 +0530

    Added basic redirecting code

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Redundant text')
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from Node.js</h1></body>');
+    res.write('</html>');
+
+    res.end();
+});
+
+server.listen(3000);
