7fa678fba87b9f66679f0c1045a81392bf53a753
Shreyans Jain <shreyansj551@gmail.com>
2019-05-29 19:16:36 +0530

    add views for more control

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,9 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
@@ -13,15 +13,15 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
-  res.redirect('/');
+  res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
+      pageTitle: "Admin Products",
+      path: "/admin/products",
       hasProducts: products.length > 0,
       activeShop: true,
       productCSS: true
