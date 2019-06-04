483daac4c86eb23dc5ce45ed17a509d7c36ab79d
Shreyans Jain <shreyansj551@gmail.com>
2019-06-04 11:22:12 +0530

    work on views

--- controllers/admin.js
+++ controllers/admin.js
@@ -34,67 +34,67 @@ exports.postAddProduct = (req, res, next) => {
   // res.redirect("/");
 };
 
-exports.getEditProduct = (req, res, next) => {
-  console.log("edit product called");
-  // using query parameters
-  const editMode = req.query.editing;
-  if (!editMode) {
-    console.log("failed in getEditProduct: product not found");
-    return res.redirect("/");
-  }
-  const prodID = req.params.productID;
-  Product.findById(prodID, product => {
-    if (!product) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true,
-      product: product
-    });
-  });
-};
+// exports.getEditProduct = (req, res, next) => {
+//   console.log("edit product called");
+//   // using query parameters
+//   const editMode = req.query.editing;
+//   if (!editMode) {
+//     console.log("failed in getEditProduct: product not found");
+//     return res.redirect("/");
+//   }
+//   const prodID = req.params.productID;
+//   Product.findById(prodID, product => {
+//     if (!product) {
+//       return res.redirect("/");
+//     }
+//     res.render("admin/edit-product", {
+//       pageTitle: "Edit Product",
+//       path: "/admin/edit-product",
+//       editing: editMode,
+//       formsCSS: true,
+//       productCSS: true,
+//       activeAddProduct: true,
+//       product: product
+//     });
+//   });
+// };
 
-exports.postEditProduct = (req, res, next) => {
-  console.log("editing product!");
+// exports.postEditProduct = (req, res, next) => {
+//   console.log("editing product!");
 
-  const productID = req.body.productID;
-  const updatedTitle = req.body.title;
-  const updatedimgURL = req.body.imgURL;
-  const updatedPrice = req.body.price;
-  const updatedDescription = req.body.description;
+//   const productID = req.body.productID;
+//   const updatedTitle = req.body.title;
+//   const updatedimgURL = req.body.imgURL;
+//   const updatedPrice = req.body.price;
+//   const updatedDescription = req.body.description;
 
-  const updatedProduct = new Product(
-    productID,
-    updatedTitle,
-    updatedimgURL,
-    updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
-  console.log("product edited!");
-  res.redirect("/admin/products");
-};
+//   const updatedProduct = new Product(
+//     productID,
+//     updatedTitle,
+//     updatedimgURL,
+//     updatedDescription,
+//     updatedPrice
+//   );
+//   updatedProduct.save();
+//   console.log("product edited!");
+//   res.redirect("/admin/products");
+// };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const productID = req.body.productID;
-  Product.deleteByID(productID);
-  res.redirect("/admin/products");
-};
+// exports.postDeleteProduct = (req, res, next) => {
+//   const productID = req.body.productID;
+//   Product.deleteByID(productID);
+//   res.redirect("/admin/products");
+// };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  });
-};
+// exports.getProducts = (req, res, next) => {
+//   Product.fetchAll(products => {
+//     res.render("admin/products", {
+//       prods: products,
+//       pageTitle: "Admin Products",
+//       path: "/admin/products",
+//       hasProducts: products.length > 0,
+//       activeShop: true,
+//       productCSS: true
+//     });
+//   });
+// };
