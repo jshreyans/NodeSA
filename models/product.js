16c1849dd9ae19f9ff3bf6f8e405bb89bc56254b
Shreyans Jain <shreyansj551@gmail.com>
2019-06-05 09:27:06 +0530

    add-product bug resolved

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ module.exports = class Product {
     this.imgURL = imgURL;
     this.description = description;
     this.price = price;
-    this._id = id;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
@@ -15,10 +15,9 @@ module.exports = class Product {
     let dbInsertProduct;
     if (this._id) {
       // update product
-      dbInsertProduct = db.collection("products").updateOne(
-        { _id: new mongodb.ObjectId(this._id) },
-        { $set: this }
-      );
+      dbInsertProduct = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
     } else {
       dbInsertProduct = db.collection("products").insertOne(this);
     }
@@ -34,8 +33,8 @@ module.exports = class Product {
   }
 
   static fetchAll() {
-    const dc = getDb();
-    return dc
+    const db = getDb();
+    return db
       .collection("products")
       .find()
       .toArray()
@@ -63,18 +62,19 @@ module.exports = class Product {
         console.log(err);
       });
   }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    db.collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(product => {
+        console.log('deleted!');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 };
-
-// const fs = require("fs");
-// const path = require("path");
-
-// const Cart = require('./cart');
-
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   "data",
-//   "products.json"
-// );
 
 // const getProductsFromFile = cb => {
 //   fs.readFile(p, (err, fileContent) => {
@@ -117,27 +117,3 @@ module.exports = class Product {
 //       }
 //     });
 //   }
-
-//   static deleteByID(id) {
-//     getProductsFromFile(products => {
-//       const product = products.find(prod => prod.id === id);
-//       const updatedProducts = products.filter(prod => prod.id !== id);
-//       fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-//         if (!err) {
-//           Cart.deleteProduct(id, product.price);
-//         }
-//       })
-//     });
-//   }
-
-//   static fetchAll(cb) {
-//     getProductsFromFile(cb);
-//   }
-
-//   static findById(id, cb) {
-//     getProductsFromFile(products => {
-//       const product = products.find(p => p.id === id);
-//       cb(product);
-//     });
-//   }
-// };
